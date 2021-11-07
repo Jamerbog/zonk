3315b57d8adf06a8075e4ede2f5d71d062cf162d
James McLeod <jamesmcleod6800@gmail.com>
2021-11-07 23:02:49 +0000

    you can run some programs accessible from PATH now :)

--- zonk.c
+++ zonk.c
@@ -8,6 +8,7 @@
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 
+int pid;
 char mode[10]; 
 
 /* Splits the string by space and returns the array of tokens
@@ -40,27 +41,25 @@ char **tokenize(char *line)
   return tokens;
 }
 
-void findFile(char * mode, char splitPath)
+void execCommand(char* mode, char ** tokens)
 {
-	while (splitPath != NULL)
-	{
-		char cwd[256];
+	pid = fork();
 
-		//printf("%s\n", splitPATH);
-		chdir(splitPath);
-		printf(getcwd(cwd, sizeof(cwd)));
-		printf("\n");
-		splitPath = strtok(NULL, ":");
+	if (pid == 0)
+	{
+		if (-1 ==  execlp(mode, mode, tokens[1], NULL)) {
+			printf("Failed to find program in PATH.");
+		}
 	}
+
 }
 
 int main(int argc, char* argv[]) {
+
 	char  line[MAX_INPUT_SIZE];            
 	char  **tokens;   
-	char* PATH = getenv("PATH");       
+  
 	int i;
-	pid_t pid;
-	char splitPath = strtok(PATH, ":");
 
 
 	while(1) {			
@@ -79,20 +78,18 @@ int main(int argc, char* argv[]) {
        //do whatever you want with the commands, here we just print them
 
 		for(i=0;tokens[i]!=NULL;i++){
+
+			strcpy(mode, tokens[0]);
+			char* temp1="Funny";
+			char* temp2="world";
 			printf("found token %s (remove this debug output later)\n", tokens[i]);
 	
-				if (strcmp(tokens[0], "cd") == 0)
-				{
-					strcpy(mode, "cd");
-
-					findFile(mode, splitPath);
-
-				}
 	
-				else
-				{
-					printf("\n");
-				}
+
+			execCommand(mode, tokens);
+
+			printf("\n");
+
 			}
 		
 		}
